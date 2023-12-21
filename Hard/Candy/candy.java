478cb97b79d0f30b5752103ec35796c081e24dc9
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2023-12-21 23:03:13 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Candy/candy.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            int ans = obj.minCandy(n, a);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int minCandy(int N, int ratings[]) {
+        // code here
+        if (ratings == null || ratings.length == 0) {
+            return 0;
+        }
+
+        int n = ratings.length;
+        int[] candies = new int[n];
+
+        // First pass: Traverse from left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Second pass: Traverse from right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Calculate the total number of candies needed
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        // Add 1 candy for each child to meet the minimum requirement
+        return totalCandies + n;
+    }
+}
