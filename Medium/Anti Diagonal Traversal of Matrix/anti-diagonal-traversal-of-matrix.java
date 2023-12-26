1de0bd55e84a7e802962aafcf299f030c34e3970
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2023-12-27 02:41:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Anti Diagonal Traversal of Matrix/anti-diagonal-traversal-of-matrix.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            
+            String input_line[] = br.readLine().trim().split("\\s+");
+            int[][] matrix = new int[n][n];
+            for(int i=0; i<n; i++) {
+                for (int j = 0; j < n; ++j) {
+                    matrix[i][j] = Integer.parseInt(input_line[i * n + j]); 
+                }
+            }
+            Solution obj = new Solution();
+            int[] ans = obj.antiDiagonalPattern(matrix);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] antiDiagonalPattern(int[][] matrix)
+    {
+       int m = matrix.length;
+        int n = matrix[0].length;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < m + n - 1; i++) {
+            for (int j = 0; j <= i; j++) {
+                int row = j;
+                int col = i - j;
+
+                if (row < m && col < n) {
+                    list.add(matrix[row][col]);
+                }
+            }
+        }
+
+        // Convert ArrayList to int array
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+
+    }
+}
